b569a33a565ef96fe4830eb1510034987110aeda
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2023-08-07 15:51:55 -0600

    CrashCourse Complete

--- Assets/EnemySkeleton.cs
+++ Assets/EnemySkeleton.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class EnemySkeleton : Entity
 {
+    bool isAttacking;
+    
     [Header("MoveInfo")]
     [SerializeField] private float moveSpeed;
+
+    [Header("Player Detection")]
+    [SerializeField] private float playerCheckDistance;
+    [SerializeField] private LayerMask whatIsPlayer;
+
+    private RaycastHit2D isPlayerDetected;
+
 
     protected override void Start()
     {
@@ -17,11 +26,45 @@ public class EnemySkeleton : Entity
     {
         base.Update();
 
+        if (isPlayerDetected)
+        {
+            if (isPlayerDetected.distance > 1)
+            {
+                rb.velocity = new Vector2(moveSpeed * 1.5f * facingDir, rb.velocity.y);
+                Debug.Log("I see Player");
+                isAttacking = false;
+            }
+            else
+            {
+                Debug.Log("Attack " + isPlayerDetected.collider.gameObject.name);
+                isAttacking = true;
+            }
+        }
+
         if (!isGrounded || isWallDetected)
             Flip();
 
+        Movement();
+    }
 
+    private void Movement()
+    {
+        if(!isAttacking)
+            rb.velocity = new Vector2(moveSpeed * facingDir, rb.velocity.y);
+    }
 
-        rb.velocity = new Vector2(moveSpeed * facingDir, rb.velocity.y);
+    protected override void CollisionChecks()
+    {
+        base.CollisionChecks();
+
+        isPlayerDetected = Physics2D.Raycast(transform.position, Vector2.right, playerCheckDistance * facingDir, whatIsPlayer);
+
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x + playerCheckDistance * facingDir, transform.position.y));
     }
 }
