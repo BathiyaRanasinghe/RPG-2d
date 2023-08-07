4c6745cc170edf5837642d680f5edc259e7913fb
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2023-08-07 15:24:34 -0600

    Moving Ai Enemy

--- Assets/Entity.cs
+++ Assets/Entity.cs
@@ -15,13 +15,22 @@ public class Entity : MonoBehaviour
     [SerializeField] protected Transform groundCheck;
     [SerializeField] protected float groundCheckDistance;
     [SerializeField] protected LayerMask whatIsGround;
-
+    [Space]
+    [SerializeField] protected Transform wallCheck;
+    [SerializeField] protected float wallCheckDistance;
+    
    protected bool isGrounded;
+   protected bool isWallDetected;
 
     protected virtual void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
+
+        if(wallCheck == null)
+            wallCheck = transform;
+
+     
     }
 
     protected virtual void Update()
@@ -32,6 +41,9 @@ public class Entity : MonoBehaviour
     protected virtual void CollisionChecks()
     {
         isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+
+        isWallDetected = Physics2D.Raycast(wallCheck.position, Vector2.right, wallCheckDistance * facingDir, whatIsGround);
+
     }
 
     protected virtual void Flip()
@@ -43,5 +55,9 @@ public class Entity : MonoBehaviour
 
      protected virtual void OnDrawGizmos() {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
+
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance * facingDir, wallCheck.position.y));
+
+        
     }
 }
