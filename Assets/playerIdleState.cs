efe5f8e1cfe210aaf8a66c8545bc5fcd9c5d26aa
Bathiya Ranasinghe <90421548+BathiyaRanasinghe@users.noreply.github.com>
2023-08-13 23:46:45 -0600

    Wall Slide

--- Assets/playerIdleState.cs
+++ Assets/playerIdleState.cs
@@ -20,10 +20,13 @@ public class PlayerIdleState : PlayerGroundedState
     
     public override void Update() {
         base.Update();
+
+        if(xInput == player.facingDir && player.IsWallDetected())
+            return;
+    
         if (xInput != 0)
-        {
             stateMachine.ChangeState(player.moveState);
-        }
+        
     }
 
 }
